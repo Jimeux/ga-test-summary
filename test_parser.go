226bb3b2216f2cee5ea29793dd9cbd1b4d854457
Jimeux <jimeux@gmail.com>
2022-05-23 22:00:34 +0900

    Try from Go code

--- test_parser.go
+++ test_parser.go
@@ -146,6 +146,9 @@ go test ./... -run '%s'
 			_, _ = fmt.Fprintf(w, "```\n\n</details>\n\n")
 		}
 	}
+
+	os.Setenv("GITHUB_STEP_SUMMARY", "# From Go Code\n\nHello")
+	os.Exit(1) // fail build
 }
 
 func summaryTable() string {
