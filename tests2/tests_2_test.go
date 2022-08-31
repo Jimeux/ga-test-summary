2eea1dfc4a05ced0fbc4c1b854e177d79d1f23ad
Jimeux <jimeux@gmail.com>
2022-08-31 19:11:05 +0900

    tests2

--- tests2/tests_2_test.go
+++ tests2/tests_2_test.go
@@ -3,14 +3,14 @@ package tests2
 import "testing"
 
 func TestTests2_First(t *testing.T) {
-	t.Fatal("failed first")
+	//t.Fatal("failed first")
 }
 
 func TestTests2_Second(t *testing.T) {
 }
 
 func TestTests2_Third(t *testing.T) {
-	t.Fatal("failed third")
+	//t.Fatal("failed third")
 }
 
 func TestTests2_FourthSkip(t *testing.T) {
